8cdd5ca784574bf089d60ec5241aaf8fe920a70e
mrkotee <mcvUjB9b>
2021-11-16 21:21:20 +0300

    add default settings file

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+botva_pass = 'pswrd'
+name_id = 1  # bot's id
+botva_login = 'somebot@gmail.com' # login in game. Changes automatically, then bot has been blocked
+
+
+bot_dir_path = '/path/to/bot/dir/'
+btl_filepath = '/path/to/battles.json'  # file to save actual battles
+
+
+skype_login = 'somebot@gmail.com'
+skype_pass = 'pswrd'
+skype_groups = ['Group_name']
